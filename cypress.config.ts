5694dc27e71473a304cf59e30683c18b348d3b97
***** <721466+soodoku@users.noreply.github.com>
2026-03-14 00:08:27 -0700

    reorg

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,13 +3,13 @@ import { setupMimiqTasks, createLocalRuntime } from "./src/node/index";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5174",
+    baseUrl: "http://localhost:5173",
     supportFile: "cypress/support/e2e.ts",
     specPattern: "cypress/e2e/**/*.cy.ts",
     video: false,
     setupNodeEvents(on, config) {
       const runtime = createLocalRuntime({
-        scenesDir: "./examples/scenes",
+        scenesDir: "./test/scenes",
       });
 
       setupMimiqTasks(on, { runtime });
